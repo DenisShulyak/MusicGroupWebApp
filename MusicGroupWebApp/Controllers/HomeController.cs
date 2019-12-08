1ce5b2ce8d8920226503498c09bcb3ae10fc080c
Denis <denshufly@mail.ru>
2019-12-08 13:35:36 +0600

    Nirvana

---
+++ MusicGroupWebApp/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicGroupWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult DownloadAlbom()
+        {
+
+            return File($"/App_Data/Nirvana/Nirvana.rar", "application/x-rar-compressed", "Nirvana.rar");
+        }
+        public ActionResult DownloadOpinion()
+        {
+
+            return File($"/App_Data/Nirvana/Opinion.mp3", "audio/mp3", "Opinion.mp3");
+        }
+        public ActionResult DownloadBeenASon()
+        {
+
+            return File($"/App_Data/Nirvana/BeenASon.mp3", "audio/mp3", "BeenASon.mp3");
+        }public ActionResult DownloadDrainYou()
+        {
+
+            return File($"/App_Data/Nirvana/DrainYou.mp3", "audio/mp3", "DrainYou.mp3");
+        }public ActionResult DownloadLithium()
+        {
+
+            return File($"/App_Data/Nirvana/Lithium.mp3", "audio/mp3", "Lithium.mp3");
+        }public ActionResult DownloadPayToPlay()
+        {
+
+            return File($"/App_Data/Nirvana/PayToPlay.mp3", "audio/mp3", "PayToPlay.mp3");
+        }public ActionResult DownloadSliver()
+        {
+
+            return File($"/App_Data/Nirvana/Sliver.mp3", "audio/mp3", "Sliver.mp3");
+        }public ActionResult DownloadWhereDidYouSleepLastNight()
+        {
+
+            return File($"/App_Data/Nirvana/WhereDidYouSleepLastNight.mp3", "audio/mp3", "WhereDidYouSleepLastNight.mp3");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MusicGroupWebApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("MusicGroupWebApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MusicGroupWebApp")]
+[assembly: AssemblyCopyright("© , 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("6e1a3fa8-6dda-438d-be2a-f7d49b437d3c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
